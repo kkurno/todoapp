ccef7d8ce5fb6a01f14db4a7d96b4dbc1645407b
kkurno <kunakorn.pgm@gmail.com>
2018-06-10 17:44:59 +0700

    change space size from 4 to 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,34 +2,34 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Footer extends Component {
-    render() {
-        // เปลี่ยนไปใช้ .map เพื่อ loop แล้วใส่เงื่อนไขทีละตัว
-        const filters = [
-            { value: 'all', label: 'All' },
-            { value: 'active', label: 'Active' },
-            { value: 'completed', label: 'Completed' },
-        ];
+  render() {
+    // เปลี่ยนไปใช้ .map เพื่อ loop แล้วใส่เงื่อนไขทีละตัว
+    const filters = [
+      { value: 'all', label: 'All' },
+      { value: 'active', label: 'Active' },
+      { value: 'completed', label: 'Completed' },
+    ];
 
-        return (
-            <footer className="footer">
-                <span className="todo-count">
-                    <strong>{this.props.countActiveItem}</strong> <span>item left</span>
-                </span>
-                <ul className="filters">
-                    {filters.map(filter => (
-                        <li key={filter.value}>
-                          <a
-                            className={filter.value === this.props.filter ? 'selected' : ''}
-                            onClick={() => this.props.changeCurrentFilter(filter.value)}
-                          >
-                            {filter.label}
-                          </a>
-                        </li>
-                    ))}
-                </ul>
-            </footer>
-        );
-    }
+    return (
+      <footer className="footer">
+        <span className="todo-count">
+          <strong>{this.props.countActiveItem}</strong> <span>item left</span>
+        </span>
+        <ul className="filters">
+          {filters.map(filter => (
+            <li key={filter.value}>
+              <a
+                className={filter.value === this.props.filter ? 'selected' : ''}
+                onClick={() => this.props.changeCurrentFilter(filter.value)}
+              >
+                {filter.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-
-
-    handleKeyUp = (e) => {
-        // const text = e.target.value;
-        if (e.keyCode === 13) {
-            this.props.onSubmit(this.inputElement.value);
-            this.inputElement.value = '';
-        }
+  handleKeyUp = (e) => {
+    // const text = e.target.value;
+    if (e.keyCode === 13) {
+      this.props.onSubmit(this.inputElement.value);
+      this.inputElement.value = '';
     }
+  }
 
-    render() {
-        return (
-            <header className="header">
-                <h1>Todo</h1>
-                <input type="text" ref={node => this.inputElement = node} className="new-todo" placeholder="" onKeyUp={this.handleKeyUp}/>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header className="header">
+        <h1>Todo</h1>
+        <input type="text" ref={node => this.inputElement = node} className="new-todo" placeholder="" onKeyUp={this.handleKeyUp}/>
+      </header>
+    );
+  }
 }
 
 export default Header;
